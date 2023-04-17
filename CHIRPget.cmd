0d845868916bf1c1fbe564be9e6b34072c5f6837
nikant <nikantgr@yahoo.gr>
2023-04-17 20:52:13 +0300

    Add files via upload

---
+++ CHIRPget.cmd
@@ -0,0 +1,34 @@
+@echo off
+rem by nikant
+setlocal enabledelayedexpansion
+
+set /a day=-1
+
+:loop
+set today=%date:~-4%%date:~3,2%%date:~0,2%
+echo.
+set /a day=%day%+1
+set /a testdate=%today%-%day%
+echo !today!
+echo.
+set url=https://trac.chirp.danplanet.com/chirp_next/next-%testdate%/chirp-next-%testdate%-win32.zip
+echo !url!
+echo.
+echo.
+start /wait wget.exe -q --spider !url!
+if errorlevel 8 (
+    if %day% lss 30 (
+        goto :loop
+    ) else (
+        echo File not found for today or the previous 30 days.
+        exit /b 1
+    )
+) else (
+    start /wait wget.exe !url! -O chirp-next-%testdate%-win32.zip
+    start /wait 7z32x.exe x chirp-next-%testdate%-win32.zip -ochirp-next-%testdate%-win32 -y
+    del chirp-next-%testdate%-win32.zip
+)
+echo 
+echo 
+echo.
+timeout /t 10
